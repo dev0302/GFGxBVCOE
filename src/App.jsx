4d97b9cdbccefaee04fb79814434f1022632637b
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-08-15 12:20:00 +0530

    Merge Conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import About from "./pages/About"
 import Team from "./pages/Team"
 import Events from "./pages/Events"
 import Navbar from "./components/Navbar"
-<<<<<<< HEAD
 import NotFound from "./components/NotFound"
-=======
 import Contact from "./pages/Contact"
->>>>>>> 4ab432d268e46033adfc9ac8a37430eca3107692
 
 function App() {
   return (
@@ -20,11 +17,8 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/team" element={<Team />} />
         <Route path="/events" element={<Events />} />
-<<<<<<< HEAD
         <Route path="/notfound" element={<NotFound></NotFound>} />
-=======
         <Route path="/contact" element={<Contact />} />
->>>>>>> 4ab432d268e46033adfc9ac8a37430eca3107692
       </Routes>
     </>
   )
