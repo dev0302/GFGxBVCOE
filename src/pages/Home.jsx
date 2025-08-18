3f8bc7dfd7cc0f71b366cdbd540b139f74200201
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-08-18 19:08:54 +0530

    Fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,7 +82,7 @@ function Home() {
           {/* Title */}
           <h1
             ref={titleRef}
-            className="text-2xl md:text-5xl font-bold text-white mb-8 tracking-tight leading-tight md:leading-tight pb-1"
+            className="text-2xl md:text-5xl font-bold text-white mb-8 tracking-tight pt-8 leading-tight md:leading-tight pb-1"
             style={{
               background: "linear-gradient(135deg, #22c55e, #10b981, #059669)",
               backgroundClip: "text",
@@ -109,7 +109,7 @@ function Home() {
             ref={btnRef}
             className="flex flex-col sm:flex-row gap-6 mt-12 font-nunito items-center"
           >
-            <NavLink to="notfound">
+            <NavLink to="https://forms.gle/2gSJCp5T7VYUV7MA7">
               <button
                 id="btn-join"
                 className="px-8 py-4 bg-gradient-to-r from-green-500 to-emerald-500 text-white font-semibold rounded-full text-lg transition-all duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 hover:shadow-lg hover:shadow-emerald-500/40"
