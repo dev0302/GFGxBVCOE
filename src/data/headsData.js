63b71225706c28af1dc3dca2ab2f3cbd0ac22b78
dev0302 <devmalik9953@gmail.com>
2025-09-12 22:10:52 +0530

    Added Vite React project files

--- src/data/headsData.js
+++ src/data/headsData.js
@@ -1,6 +1,108 @@
-// Temporary placeholder for Heads. Replace with real data later.
-const headsData = [];
+const headsData = [
+    {
+        name: "Priyanshu Tiwari",
+        branch: "ECE",
+        year: "2nd",
+        position: "C &D Head",
+        p0: "C &D Head",
+        image: "/Priyanshu.webp",
+        p1: "Executive last year",
+        p2: "",
+        email: "priyanshutiwari1710@gmail.com",
+        instaLink: "https://www.instagram.com/priyanshu_btdb/",
+        linkedinLink: "https://www.linkedin.com/in/priyanshu-tiwari-8b79b5328"
+    },
+    {
+        name: "Niharika Arora",
+        branch: "CSE",
+        year: "2nd",
+        position: "Videography Head",
+        p0: "Videography Head",
+        image: "/Niharika.webp",
+        p1: "",
+        p2: "",
+        email: "Niharikaarora224@gmail.com",
+        instaLink: "@_nihar1ka_",
+        linkedinLink: "https://www.linkedin.com/in/niharika-arora-22n442?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+    },
+    {
+        name: "Dhruv Kapoor",
+        branch: "ECE",
+        year: "2nd",
+        position: "Videography and Editing Head",
+        p0: "Videography and Editing Head",
+        image: "/Dhruv.webp",
+        p1: "nil",
+        p2: "nil",
+        email: "kapoordhruv014@gmail.com",
+        instaLink: "@dhrruuvvv",
+        linkedinLink: "https://www.linkedin.com/in/dhruv-kapoor-3391b0358?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+    },
+    {
+        name: "Bharti",
+        branch: "CSE",
+        year: "2nd",
+        position: "EM Head",
+        p0: "EM Head",
+        image: "/Bharti.webp",
+        p1: "Executive",
+        p2: "",
+        email: "bhartib.2109@gmail.com",
+        instaLink: "https://www.instagram.com/bharti.2831?igsh=MW43NDhoNmZybTFiYg==",
+        linkedinLink: "https://www.linkedin.com/in/bharti-a86b8b36b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+    },
+    {
+        name: "Rameshwar",
+        branch: "CSE",
+        year: "3rd",
+        position: "Tech Head",
+        p0: "Tech Head",
+        image: "/Rameshwar.webp",
+        p1: "No",
+        p2: "",
+        email: "rameshwar.bvcoe092@gmail.com",
+        instaLink: "",
+        linkedinLink: "https://www.linkedin.com/in/rameshwar-7b8905292?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+    },
+    {
+        name: "Shreyans Kumar Singh",
+        branch: "ECE",
+        year: "2nd",
+        position: "Design Head",
+        p0: "Design Head",
+        image: "/Shreyans.webp",
+        p1: "",
+        p2: "",
+        email: "mshrayans@gmail.com",
+        instaLink: "https://www.instagram.com/shrayans.02/",
+        linkedinLink: "https://www.linkedin.com/in/shrayans-kumar-singh-2a8293325/"
+    },
+    {
+        name: "Pururaj Singh",
+        branch: "ECE",
+        year: "3rd",
+        position: "Design and Creatives Head",
+        p0: "Design and Creatives Head",
+        image: "/Pururaj.webp",
+        p1: "Design Executive",
+        p2: "",
+        email: "pururajsingh76@gmail.com",
+        instaLink: "https://www.instagram.com/pururajsinghrajput/",
+        linkedinLink: "https://www.linkedin.com/in/pururaj-singh-9b91a22bb/"
+    },
+    {
+        name: "Krishna Bansal",
+        branch: "CSE",
+        year: "2nd",
+        position: "PR & Outreach Head",
+        p0: "PR & Outreach Head",
+        image: "/Krishna.webp",
+        p1: "Marketing Executive",
+        p2: "Marketing Executive",
+        email: "bansalkrishnaa835@gmail.com",
+        instaLink: "https://www.instagram.com/krishnaa.ww?igsh=em9mbWh6YWR2eWhw",
+        linkedinLink: "https://www.linkedin.com/in/krishna-bansal-814252317/"
+    }
+];
 
 export default headsData;
-
-
