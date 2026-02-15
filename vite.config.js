49f873fae86216a473c4fe8af3b2146c74110d8e
Dev0302 <devmalik9953@gmail.com>
2026-02-16 00:31:53 +0530

    meaningful message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // ✅ use relative paths so assets load correctly on Hostinger
+  base: '/', // ✅ use relative paths so assets load correctly on Hostinger
   plugins: [react()],
   build: {
     outDir: 'dist', // default, but added for clarity
