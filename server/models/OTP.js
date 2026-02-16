8d09c58586ead1ddf8e719140492c281793e993c
Dev0302 <devmalik9953@gmail.com>
2026-02-16 13:18:38 +0530

    meaningful message

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -4,8 +4,12 @@ const otpSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, trim: true, lowercase: true },
     otp: { type: String, required: true },
-  },
-  { timestamps: true }
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 5 * 60 // OTP expires in 5 minutes
+    }
+  }
 );
 
 module.exports = mongoose.model("OTP", otpSchema);
