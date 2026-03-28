19088af3416419dae1a6066b788554de4d5c47fb
Dev0302 <devmalik9953@gmail.com>
2026-03-28 16:34:01 +0530

    em drag and drop feature

--- src/components/ui/shining-text.jsx
+++ src/components/ui/shining-text.jsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react";
 export function ShiningText({ text, className = "" }) {
   return (
     <motion.h1
-      className={`bg-[linear-gradient(110deg,rgba(147,51,234,0.35),35%,rgba(233,213,255,0.95),50%,rgba(147,51,234,0.35),75%,rgba(147,51,234,0.35))] bg-[length:200%_100%] bg-clip-text text-base font-regular text-transparent ${className}`}
+     className={`bg-[linear-gradient(110deg,rgba(15,118,110,0.9),35%,rgba(153,246,228,1),50%,rgba(20,184,166,0.9),75%,rgba(15,118,110,0.9))] bg-[length:200%_100%] bg-clip-text text-base font-regular text-transparent ${className}`}
       initial={{ backgroundPosition: "200% 0" }}
       animate={{ backgroundPosition: "-200% 0" }}
       transition={{
