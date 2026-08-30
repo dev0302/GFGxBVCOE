3a45bc70e20e2d78996cfc962d3f1280346b1386
Dev Malik <devmalik9953@gmail.com>
2026-08-30 19:38:59 +0530

    Display temporary downtime message in main component

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <div className="text-white">This Site is Temporarily Down</div>
         </BrowserRouter>
       </PersistGate>
     </Provider>
